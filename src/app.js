7ce2e706af2fc1df60e9412d2b702c31836d25ea
mekatronnur <92737096+mekatronnur@users.noreply.github.com>
2021-10-19 15:01:37 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,32 @@
+console.log("merhaba kodlama.io")
+
+
+
+
+let dolarDun = 9.20
+
+let dolarBugun =9.30
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+
+
+// array dizi köşeli parantez şekil karekter sayı eklenebilir.
+//camelcasing değişken tanımlama fonksiyonn tanımlama gibi 
+//pascalCasing değişken tanımlama
+let konutkredileri = [ "konut kredisi","emlak konut kredisi","kamu konut kredisi"," özel kamu konut kredisi"]
+//react ön yüz geliştirme kütüphanesi
+
+console.log("<ul>")
+for(let i = 0;i<konutkredileri.length; i++){
+    console.log("<li>"+konutkredileri[i]+"</li>")
+}
+
+console.log("</ul>")
+
+
+console.log(konutkredileri)
